a5cf1475b1278a0b1ccdfccf582d48af5e2a78d5
Ezequiel <eoggioni@alphactory.com.ar>
2025-05-05 21:27:02 -0300

    Services input output

--- src/app/Paginas/lista-producto/lista-producto.component.ts
+++ src/app/Paginas/lista-producto/lista-producto.component.ts
@@ -5,18 +5,20 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { ProductoDetalleComponent } from "../producto-detalle/producto-detalle.component";
+import { ProductoService } from '../../servicios/producto.service';
 
 @Component({
   selector: 'app-lista-producto',
-  imports: [MatListModule, CommonModule,MatCardModule, MatIconModule],
+  imports: [MatListModule, CommonModule, MatCardModule, MatIconModule, ProductoDetalleComponent],
   templateUrl: './lista-producto.component.html',
   styleUrl: './lista-producto.component.css'
 })
 export class ListaProductoComponent {
   public productos: Array<Producto>;
 
-  constructor(public route:Router) {
-    this.productos = JSON.parse(localStorage.getItem("productos") ?? '[]');
+  constructor(public route:Router, public productoService:ProductoService ) {
+    this.productos = this.productoService.Producto;
   }
 
   public editar(id:number){
--- src/app/Paginas/producto/producto.component.ts
+++ src/app/Paginas/producto/producto.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../../servicios/producto.service';
 
 @Component({
   selector: 'app-producto',
@@ -15,34 +16,37 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductoComponent {
   public miProducto!: Producto;
 
-  constructor(public activeRoute: ActivatedRoute) {
-   
+  constructor(public activeRoute: ActivatedRoute, public productoService: ProductoService) {
+
     this.activeRoute.paramMap.subscribe(
       param => {
         let idParametro = param.get("idProducto") ?? "";
         if (idParametro == "")
           this.miProducto = new Producto();
         else {
-          let productos: Array<Producto> = JSON.parse(localStorage.getItem("productos") ?? '[]');
-          this.miProducto = productos.filter(prod => prod._id.toString() == idParametro)[0]?? new Producto();
+          this.miProducto = this.productoService.Producto.filter(prod => prod._id.toString() == idParametro)[0] ?? new Producto();
         }
       }
     );
   }
 
+  public log(argumento: any) {
+    console.info(argumento);
+  }
+
   public guardar() {
-    let productos: Array<Producto> = JSON.parse(localStorage.getItem("productos") ?? '[]');
-   if(this.miProducto._id){
-    let anterior = productos.filter(prod => prod._id == this.miProducto._id)[0];
-    anterior._nombre = this.miProducto._nombre;
-    anterior._moneda = this.miProducto._moneda;
-    anterior._precio = this.miProducto._precio;
-    anterior._tipoProducto = this.miProducto._tipoProducto;
-   }else{
-     this.miProducto.id = productos.length + 1;
-     productos.push(this.miProducto);
-   }
-    localStorage.setItem('productos', JSON.stringify(productos));
+    let productos: Array<Producto> = this.productoService.Producto;
+    if (this.miProducto._id) {
+      let anterior = productos.filter(prod => prod._id == this.miProducto._id)[0];
+      anterior._nombre = this.miProducto._nombre;
+      anterior._moneda = this.miProducto._moneda;
+      anterior._precio = this.miProducto._precio;
+      anterior._tipoProducto = this.miProducto._tipoProducto;
+    } else {
+      this.miProducto.id = productos.length + 1;
+      productos.push(this.miProducto);
+    }
+    this.productoService.saveProducto(productos);
   }
 
   public limpiar() {
